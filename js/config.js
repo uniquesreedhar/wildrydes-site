d7b9d5e110e4ffaaee6e6b76396f2929a1b2970a
uniquesreedhar <97074135+uniquesreedhar@users.noreply.github.com>
2025-04-21 21:25:15 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://pve2ew8m2k.execute-api.us-east-1.amazonaws.com/QA' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
