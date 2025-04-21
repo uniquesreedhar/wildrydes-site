adcb2a52076c2c0c7a125611b5bc53d2a3dc0fba
uniquesreedhar <97074135+uniquesreedhar@users.noreply.github.com>
2025-04-21 16:11:17 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ohDMYWXWd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2fbrq89psfk4g9d0fhos9e8tpu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_pvVaz7yec', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3sgnvbiq1dcl0j1041nicf1f0k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
