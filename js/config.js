9db0391862c08fb864636887f54300f508f641bf
uniquesreedhar <97074135+uniquesreedhar@users.noreply.github.com>
2025-04-21 19:05:20 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_RNhWpg77R', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '46i36ip2ljadi755q9iac5sv6h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'R', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://pve2ew8m2k.execute-api.us-east-1.amazonaws.com/QA' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
